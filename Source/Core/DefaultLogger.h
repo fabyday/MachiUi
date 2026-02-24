24eb56a532a690bd38e5b00dff4f94db6dd8c686
yabai <rohjihyun95@gmail.com>
2026-02-24 09:38:17 +0900

    add log

--- Source/Core/DefaultLogger.h
+++ Source/Core/DefaultLogger.h
@@ -14,7 +14,7 @@ public:
     virtual void LogInfo(const std::string &msg) = 0;
     virtual void LogWarn(const std::string &msg) = 0;
     virtual void LogError(const std::string &msg) = 0;
-
+    
     virtual void LogWarn(const std::string &msg) = 0;
     virtual void LogError(const std::string &msg) = 0;
 };
